981a2a8c78e5e5c9c97ecdc46abf87189ed8e8ae
Uwaifo Ilaboya <uwaifoilaboya@Uwaifos-MacBook-Pro.local>
2023-03-22 14:59:16 -0500

    david-changes

--- backend.tf
+++ backend.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 0.12.6"
   
   backend "azurerm" {
-    subscription_id      = "<subscription>"
+    subscription_id      = "cdc883e8-695a-42c8-a91d-d37379e70e8b"
     resource_group_name  = "wizDemoResourceGroup"
-    storage_account_name = "wizpipelinestorage"
+    storage_account_name = "cs71003200240201aaf"
     container_name       = "terraformstate"
     key                  = "root.terraform.tfstate"
   }
--- providers.tf
+++ providers.tf
@@ -6,6 +6,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
+      subscription_id="cdc883e8-695a-42c8-a91d-d37379e70e8b"
     }
     random = {
       source  = "hashicorp/random"
